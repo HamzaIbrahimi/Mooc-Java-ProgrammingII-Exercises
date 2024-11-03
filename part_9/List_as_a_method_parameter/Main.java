532007c996fa2d75fa585d5c5a0f18cb4a529f6a
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2024-11-03 20:48:06 +0100

    Add solution

---
+++ part_9/List_as_a_method_parameter/Main.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.function.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+class Main {
+
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("First");
+        names.add("Second");
+        names.add("Third");
+        Function<List<String>, Integer> returnSizeList = list -> list.size();
+        System.out.println(returnSizeList.apply(names));
+
+        Function<Map<String, String>, Integer> returnSizeMap = map -> map.size();
+        Map<String, String> names1 = new HashMap<>();
+        names1.put("1", "first");
+        names1.put("2", "second");
+        System.out.println(returnSizeMap.apply(names1));
+
+        Set<String> setShow = new HashSet<>();
+        setShow.add("one");
+        setShow.add("one");
+        setShow.add("two");
+        Consumer<Set<String>> showSet = set -> set.forEach(System.out::println);
+        showSet.accept(setShow);
+
+        Function<Set<String>, Integer> setSize = set -> set.size();
+        Set<String> sizeSet = new HashSet<>();
+        sizeSet.add("first");
+        sizeSet.add("first");
+        sizeSet.add("second");
+        sizeSet.add("second");
+        sizeSet.add("second");
+        System.out.println(setSize.apply(sizeSet));
+
+    }
+}
