a94781683eaecb11694bcd54dc333e043f84d476
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2024-11-12 17:08:21 +0100

    Test sublist

--- part_10/The_comparable_interface/literacy_comparison/Main.java
+++ part_10/The_comparable_interface/literacy_comparison/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         FileReader fileReader = new FileReader("literacy.csv");
         List<String> sortedFile = fileReader.sortAccordingToPopulation();
-        sortedFile.forEach(v -> System.out.println(v));
+        sortedFile.subList(0, 15).forEach(v -> System.out.println(v));
 
     }
 }
