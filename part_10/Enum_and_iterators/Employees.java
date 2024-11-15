eb66ea6a800fac7e172bfc627d992989c6dba1f7
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2024-11-15 15:51:45 +0100

    Add solution

---
+++ part_10/Enum_and_iterators/Employees.java
@@ -0,0 +1,43 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Employees {
+
+    private List<Person> employeesList;
+
+    public Employees() {
+        this.employeesList = new ArrayList<>();
+    }
+
+    public void add(Person personToAdd) {
+        this.employeesList.add(personToAdd);
+    }
+
+    public void add(List<Person> peopleToAdd) {
+        peopleToAdd.forEach(person -> {
+            this.employeesList.add(person);
+        });
+    }
+
+    public void print() {
+        this.employeesList.forEach(System.out::println);
+    }
+
+    public void print(Education education) {
+        this.employeesList.stream()
+                .filter(person -> person.getEducation() == education)
+                .forEach(System.out::println);
+    }
+
+    public void fire(Education education) {
+        Iterator<Person> iterate = this.employeesList.iterator();
+        while (iterate.hasNext()) {
+            if (iterate.next().getEducation() == education) {
+                iterate.remove();
+            }
+        }
+
+    }
+
+}
