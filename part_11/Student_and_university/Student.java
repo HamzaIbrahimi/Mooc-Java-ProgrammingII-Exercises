91ed314a911a6b8aab869d260854049434f0e0b2
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2024-11-21 21:08:20 +0100

    Add solution

---
+++ part_11/Student_and_university/Student.java
@@ -0,0 +1,6 @@
+public class Student {
+
+    private University university;
+    private String name;
+    private int studentID;
+}
---
+++ part_11/Student_and_university/University.java
@@ -0,0 +1,6 @@
+import java.util.List;
+
+public class University {
+    private String nameOfUniversity;
+    private List<Student> students;
+}
